f3c09b2cb538f4de5791c94be7bf7eb231a6bc9d
robin <robin@rhrk.uni-kl.de>
2025-07-15 08:31:37 +0200

    One Script for everything

--- testMain.py
+++ testMain.py
@@ -1,20 +1,33 @@
 # main.py
+from gripper_control import gripper_open, gripper_close
 from robot_teaching import teach_positions
 from moveFun import send_pose_to_robot
 import time
 
 # Erfasse z. B. 3 Posen vom Benutzer per Freedrive
-positionen = teach_positions(num_positions=3)
+positionen = teach_positions(num_positions=2)
 
 # Weiterverarbeitung
 print("Erfasste Positionen:")
 for i, pose in enumerate(positionen):
     print(f"Pose {i+1}: {pose}")
 
-# Fahre jede Pose 2x an
-for _ in range(2):
+# Fahre jede Pose 1x an
+for _ in range(1):
     for i, pose in enumerate(positionen):
         print(f"Fahre zu Pose {i+1}...")
-        send_pose_to_robot(pose)
+        send_pose_to_robot(pose,acceleration=0.8,velocity=1.2)
         print("Ziel erreicht")
         #time.sleep(2)  # Optional: kurze Pause zwischen Bewegungen
+
+
+
+print("Öffne Greifer...")
+gripper_open()
+
+# ggf. Wartezeit einfügen
+import time
+time.sleep(2)
+
+print("Schließe Greifer...")
+gripper_close()
